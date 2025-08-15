6d3f8322487be674a031199c34c795fbbdf5b5dd
AldarUlyumdzhiev <aldarulyumdzhiev@yandex.ru>
2025-08-15 18:12:45 +0300

    fix: fix device type deserialization from hub router

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/dto/HubEventDto.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/dto/HubEventDto.java
@@ -1,20 +1,22 @@
 package ru.yandex.practicum.kafka.telemetry.collector.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import java.time.Instant;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class HubEventDto {
     private String hubId;
     private Instant timestamp;
 
-    @JsonAlias({"kind", "eventType"})
+    @JsonAlias({"kind", "eventType", "type"})
     private HubEventKind kind;
 
     @JsonAlias({"deviceId", "id"})
     private String deviceId;
 
-    @JsonAlias({"deviceType", "type"})
+    @JsonAlias({"deviceType"})
     private DeviceTypeDto deviceType;
 }
